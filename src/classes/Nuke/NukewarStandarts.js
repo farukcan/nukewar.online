470dba45a6fb1daed7b170006e5a5aed1bbea827
Faruk CAN <omer@farukcan.net>
2026-05-13 22:00:52 +0300

    fix: Adjust NukewarStandarts costs by modifying the base variable to improve game balance

--- src/classes/Nuke/NukewarStandarts.js
+++ src/classes/Nuke/NukewarStandarts.js
@@ -1,9 +1,9 @@
-var _r$ = 60*1000*1/3;
+var _r$ = 60*1000*1/6;
 
 var NukewarStandarts = {
 	SwapCost : (3*_r$) ,
 	ReloadCost : (5*_r$) ,
 	BuildCost : (4*_r$) ,
 	ClearCost : (1*_r$) ,
-	AirDefenseCost : (4*_r$) / 10
+	AirDefenseCost : (4*_r$)
 };
